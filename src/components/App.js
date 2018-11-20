4f48e78bc01155d351b9adb4483b015517433d57
Freddie <fredjlawson@gmail.com>
2018-11-20 15:29:40 -0500

    feat: makes initial fetch call

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,42 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../style/App.css';
 
+const listId = process.env.REACT_APP_TRELLO_LIST_ID
+const token = process.env.REACT_APP_TRELLO_API_TOKEN
+const key = process.env.REACT_APP_TRELLO_API_KEY
+const cardParameters = 'fields=id,name,desc'
+const url = `https://api.trello.com/1/lists/${listId}/cards?${cardParameters}&key=${key}&token=${token}`
+
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      cards: []
+    }
+  }
+
+  // so perhaps I want to store the returned object against component state
+
+  componentDidMount() {
+    const that = this
+    fetch(url)
+    .catch(error => console.log('BAD', error))
+	  .then(function (response) {
+		  return response.json();
+    })
+    .then(function (data) {
+      // console.log(data)
+      that.setState({cards: data})
+    })
+    console.log(that.cards)
+  }
+
   render() {
-    console.log(process.env.REACT_APP_TRELLO_LIST_ID)
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          
           <p>
             The List ID is {process.env.REACT_APP_TRELLO_LIST_ID} <br/>
             The Token is {process.env.REACT_APP_TRELLO_API_TOKEN}
@@ -18,5 +47,13 @@ class App extends Component {
     );
   }
 }
+
+// So I want this component to...
+// Make an API call
+// With each 'object' returned in the response, load a card component, passing in the relevant props(?)
+
+// I question I have is, when do I make the fetch call.. hmm...
+// perhaps componentDidMount or something like that
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// So this file uses a render method on the ReactDOM (object?), passing in two arguments
+// The component to render, and the element on the DOM to render it on
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
